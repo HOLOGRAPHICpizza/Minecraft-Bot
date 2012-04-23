00732e27b819879847ff9025523564ff9ca3da51
Michael Craft <mcraft@peak15.org>
2012-04-23 13:01:58 -0400

    Fixed crap.

--- src/net/nevercast/minecraft/bot/structs/SlotData.java
+++ src/net/nevercast/minecraft/bot/structs/SlotData.java
@@ -1,29 +1,50 @@
-/*
- * Updated March 10, 2012
- * By: mikecyber 
- * For: Protocol 1.2.3 Compliance
- */
 package net.nevercast.minecraft.bot.structs;
 
+import java.io.DataInputStream;
+import java.io.IOException;
+
 /**
- * Created by IntelliJ IDEA.
- * User: Josh
- * Date: 8/15/11
- * Time: 3:13 AM
- * To change this template use File | Settings | File Templates.
+ * Represents an item or stack of items in a window slot.
+ * @see http://wiki.vg/Slot_Data
+ * @author Michael Craft <mcraft@peak15.org>
+ * @author mikecyber
+ * @author Josh
  */
-public class ItemStack {
+public class SlotData {
 
-    public final static ItemStack EMPTY = new ItemStack((short)0, (byte)0, (short)0);
+    public final static SlotData EMPTY = new SlotData((short) 0, (byte)0, (short)0);
 
-    public ItemStack(){}
-    public ItemStack(short itemid, byte count ,short damage){
+    public SlotData(){}
+    public SlotData(short itemid, byte count ,short damage){
         id = itemid;
         this.count = count;
         this.damage = damage;
+    }
+    
+    /**
+     * Constructs a slot data object from an input stream.
+     * @param objectInput Input stream to read from.
+     * @throws IOException if the data could not be read.
+     */
+    public SlotData(DataInputStream objectInput) throws IOException {
+    	//TODO: This currently has the potential to fail horribly if the data is enchantable.
+    	// See: http://wiki.vg/Slot_Data
+    	
+    	id = objectInput.readShort();
+        if(id == -1){
+            count = 0;
+            damage = 0;
+        }else{
+        	count = objectInput.readByte();
+        	damage = objectInput.readShort();
+        }
     }
 
     public short id;
     public byte count;
     public short damage;
+    
+    public String toString() {
+    	return "id:" + id + " count:" + count + " damage:" + damage;
+    }
 }
