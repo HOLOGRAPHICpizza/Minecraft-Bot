274e03fa0167b16597351a8419fd6b547c50c261
Michael Craft <mcraft@peak15.org>
2012-04-23 13:32:12 -0400

    Implemented packet 0xFA. All packets now recognized! This is now "1.2.5 compliant"! On to bug squashing! (probably)

--- src/net/nevercast/minecraft/bot/network/PacketFactory.java
+++ src/net/nevercast/minecraft/bot/network/PacketFactory.java
@@ -82,7 +82,7 @@ public class PacketFactory {
         unsupportedPackets.put((byte)0xC8, 5); 										// Increment Statistic
         supportedPackets.put((byte)0xC9, PacketC9PlayerListItem.class);
         unsupportedPackets.put((byte)0xCA, 4);										// Player Abilities
-        //supportedPackets.put((byte)0xFA, PacketFAPluginMessage.class);
+        supportedPackets.put((byte)0xFA, PacketFAPluginMessage.class);
         supportedPackets.put((byte)0xFF, PacketFFDisconnect.class);
     }
 
---
+++ src/net/nevercast/minecraft/bot/network/packets/PacketFAPluginMessage.java
@@ -0,0 +1,76 @@
+package net.nevercast.minecraft.bot.network.packets;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+import net.nevercast.minecraft.bot.MinecraftClient;
+import net.nevercast.minecraft.bot.network.IPacket;
+
+/**
+ * Two-way packet for plugin communication.
+ * @author Michael Craft <mcraft@peak15.org>
+ */
+public class PacketFAPluginMessage implements IPacket {
+	String channel;		// Plugin Channel
+	byte[] data;		// Data
+	
+	/**
+     * Get the plugin channel.
+     * @return Plugin Channel
+     */
+    public String getChannel(){
+        return channel;
+    }
+    
+    /**
+     * Get the data.
+     * @return Data
+     */
+    public byte[] getData(){
+        return data;
+    }
+	
+    /**
+     * Set the plugin channel.
+     * @param channel Plugin Channel
+     */
+    public void setChannel(String channel) {
+    	this.channel = channel;
+    }
+    
+    /**
+     * Set the data.
+     * @param data Data
+     */
+    public void setData(byte[] data) {
+    	this.data = data;
+    }
+    
+	@Override
+	public byte getPacketId() {
+		return (byte) 0x83;
+	}
+
+	@Override
+	public void writeExternal(DataOutputStream objectOutput) throws IOException {
+		MinecraftClient.writeString(objectOutput, channel);
+		objectOutput.writeShort(data.length);
+		objectOutput.write(data);
+	}
+
+	@Override
+	public void readExternal(DataInputStream objectInput) throws IOException {
+		channel = MinecraftClient.readString(objectInput);
+		
+		short length = objectInput.readShort();
+		data = new byte[length];
+		objectInput.readFully(data);
+	}
+
+	@Override
+	public String log() {
+		return "@ 0x83 channel=" + channel + " length=" + data.length;
+	}
+
+}
